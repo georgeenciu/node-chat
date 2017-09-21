8b6fd283444f9060cb8f0311e53165e44f84fe8b
George Enciu <george.enciu@gmail.com>
2017-09-21 16:24:56 +0300

    init

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+
+app.listen(PORT, () => {
+    console.log('started on 3000...');
+});
